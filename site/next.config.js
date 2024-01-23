ba66800b23abc47c7bdc36009d809b03ef5805d2
JClark2T <108725412+JClark2T@users.noreply.github.com>
2024-01-23 14:39:39 +0000

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: '2T Kasm Registry',
+    description: 'Workspace Registry for SSOC Kasm Instances',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://jclark2t.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/JClark2T/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
